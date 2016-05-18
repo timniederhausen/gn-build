3ea5a50ae622ca4a433330d940d9d8f28d33bd53
Tim Niederhausen <tim@rnc-ag.de>
2016-05-18 23:41:11 +0200

    Fix manifest_wrapper.py path and add manifest_to_rc.py tool

---
+++ toolchain/win/manifest_to_rc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+import os
+
+def main(manifest_path, resource_path, resource_name):
+  """Creates a resource file pointing a SxS assembly manifest.
+  |args| is tuple containing path to resource file, path to manifest file
+  and resource name which can be "1" (for executables) or "2" (for DLLs)."""
+  with open(resource_path, 'wb') as output:
+    output.write('#include <windows.h>\n%s RT_MANIFEST "%s"' % (
+      resource_name,
+      os.path.abspath(manifest_path).replace('\\', '/')))
+
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main(*sys.argv[1:]))
