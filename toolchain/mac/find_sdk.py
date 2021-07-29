771cc7d49dcc419372bbc9183aa27a343d327fa2
Andrew Voznytsa <andrew.voznytsa@gmail.com>
2021-07-29 21:26:23 +0300

    find_sdk.py: enable macOS SDK 11.x support

--- toolchain/mac/find_sdk.py
+++ toolchain/mac/find_sdk.py
@@ -55,6 +55,7 @@ def main():
   sdk_dir = os.path.join(
       out.rstrip(), 'Platforms/MacOSX.platform/Developer/SDKs')
   sdks = [re.findall('^MacOSX(10\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
+  sdks += [re.findall('^MacOSX(11\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
   sdks = [s[0] for s in sdks if s]  # [['10.5'], ['10.6']] => ['10.5', '10.6']
   sdks = [s for s in sdks  # ['10.5', '10.6'] => ['10.6']
           if (parse_version(s) >= parse_version(min_sdk_version))]
