1f2486e47387209903486a8c064448615058b2bd
Andrew Voznytsa <andrew.voznytsa@gmail.com>
2021-07-31 19:53:02 +0300

    filter_libtool.py: convert output to UTF-8

--- toolchain/mac/filter_libtool.py
+++ toolchain/mac/filter_libtool.py
@@ -25,7 +25,7 @@ def Main(cmd_list):
   env['ZERO_AR_DATE'] = '1'
   libtoolout = subprocess.Popen(cmd_list, stderr=subprocess.PIPE, env=env)
   _, err = libtoolout.communicate()
-  for line in err.splitlines():
+  for line in err.decode("utf-8").splitlines():
     if not libtool_re.match(line) and not libtool_re5.match(line):
       print(line, file=sys.stderr)
   # Unconditionally touch the output .a file on the command line if present
