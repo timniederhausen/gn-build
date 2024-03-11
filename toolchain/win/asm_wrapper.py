38814e7af7802e104c2c95f226895141e7751e2b
Tim Niederhausen <tim@rnc-ag.de>
2024-03-11 18:12:47 +0100

    feat: Add support for llvm-ml and armasm64.exe
    armasm64.exe is Microsoft's reference compiler for 64-bit Arm assembly code.
    This is needed to make sure that V8 can be tested and cross-compiled
    under MSVC for Windows on Arm systems.

--- toolchain/win/asm_wrapper.py
+++ toolchain/win/asm_wrapper.py
@@ -7,6 +7,11 @@ from toolchain import GetEnv
 def main(arch, *args):
   """Filter logo banner from invocations of asm.exe."""
   env = GetEnv(arch)
+  if sys.platform == 'win32':
+    # Windows ARM64 uses clang-cl as assembler which has '/' as path
+    # separator, convert it to '\\' when running on Windows.
+    args = list(args) # *args is a tuple by default, which is read-only
+    args[0] = args[0].replace('/', '\\')
   popen = subprocess.Popen(args, shell=True, env=env, universal_newlines=True,
                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   out, _ = popen.communicate()
